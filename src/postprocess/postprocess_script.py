736a1ca1548a69ffacd5b06574f7d2a4e1229fad
Vlad Y <vladjagnion@gmail.com>
2025-08-13 22:24:58 +0100

    many changes most important fixed  is_anchor=False in folder_submission() inside submissions.py to calculate bitrate properly otherwise it was givin 0 all the time because it was comparing pipeline to itslef...

--- src/postprocess/postprocess_script.py
+++ src/postprocess/postprocess_script.py
@@ -3,11 +3,18 @@ import numpy as np
 
 def postprocess(input_img: Frame):
     #FIXME
-    # You can do postprocessing here
-    input_img.image = np.where(input_img.image != 255, input_img.image + input_img.metadata.offset, 255)
+    # # You can do postprocessing here
+    # input_img.image = np.where(input_img.image != 255, input_img.image + input_img.metadata.offset, 255)
     
-    #check that values are still correct
-    input_img.image = np.clip(input_img.image, 0, 255)
+    # #check that values are still correct
+    # input_img.image = np.clip(input_img.image, 0, 255)
+
+    # Add offset from metadata where pixel is not 255
+    img = np.where(input_img.image != 255, input_img.image + input_img.metadata.offset, 255)
+    # Add mild random noise
+    noise = np.random.randint(0, 2, img.shape, dtype=np.uint8)
+    img = np.clip(img + noise, 0, 255).astype(np.uint8)
+    input_img.image = img
     return input_img
 
 def main(input_img):
